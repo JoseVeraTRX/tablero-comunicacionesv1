f582ee80162fa24709974b701ad5241c348b6442
JoseVeraTRX <josemanuelveralopera@pascualbravo.edu.co>
2025-04-06 21:39:07 -0500

    change in base to export in github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+//https://tablero-comunicacionesv1/
 export default defineConfig({
+  base: '/tablero-comunicacionesv1/',
   plugins: [react()],
 })
